7eb029f3a3e7705b8e5ece1b071c3745ad3ec9d0
santhoshr <santhoshr@live.com>
2024-12-19 16:57:45 +0530

    Config updates

---
+++ custom.el
@@ -0,0 +1,20 @@
+;;; custom.el --- Emacs Customization -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("98b4ef49c451350c28a8c20c35c4d2def5d0b8e5abbc962da498c423598a1cdd" default))
+ '(org-fold-core-style 'overlays)
+ '(package-selected-packages
+   '(corfu nerd-icons-corfu embark-consult embark marginalia orderless vertico gptel elfeed deft hyperbole magit compile-angel)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2E3440" :foreground "#D8DEE9" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 150 :width normal :foundry "nil" :family "Menlo")))))
+
+;;; custom.el ends here
---
+++ post-init.el
@@ -0,0 +1,5 @@
+;;; post-init.el --- Post Init -*- no-byte-compile: t; lexical-binding: t; -*-
+
+(load-file "~/dex/Vocab/Config/EmacsRCInitFile")
+
+;;; post-init.el ends here
